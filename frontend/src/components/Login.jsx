ab353061a104ac7ab6fecad67f5430743efe0ca4
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2023-04-30 20:38:38 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,47 +1,54 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import Button from "./Button";
-import { Link,useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
-const Login = ({SERVER_URL}) => {
+const Login = ({ SERVER_URL }) => {
 
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
     const navigate = useNavigate()
 
-    const [showPass,setShowPass] = useState(true)
+    const [showPass, setShowPass] = useState(true)
 
     // handle show password
-    const handleShowPass =()=>{
+    const handleShowPass = () => {
         setShowPass(!showPass)
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         const auth = localStorage.getItem("user")
-        if(auth){
+        if (auth) {
             navigate('/')
         }
-    },[])
+    }, [])
 
-    const getData=async() =>{
+    const loginUser = async () => {
 
-       
-        let result =await fetch(`${SERVER_URL}/login`,{
-            method: 'POST',
-            body: JSON.stringify({email,password}),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        result =await result.json()
+        if (email && password) {
 
-        console.warn(result);
-        if(result.name){
-            localStorage.setItem('user',JSON.stringify(result))
-            navigate('/')
-            alert("Login Successful")
-        }else{
-            alert("Please enter correct details")
+            let result = await fetch(`${SERVER_URL}/login`, {
+                method: 'POST',
+                body: JSON.stringify({ email, password }),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+            result = await result.json()
+
+            console.warn(result);
+            if (result.name) {
+                localStorage.setItem('user', JSON.stringify(result))
+                navigate('/')
+                alert("Login Successful")
+            } else {
+                alert("Please enter correct details")
+            }
+
         }
+        else{
+            alert("Please enter all details")
+        }
+
 
     }
 
@@ -51,21 +58,21 @@ const Login = ({SERVER_URL}) => {
                 <h1 className="font-bold text-2xl text-center my-2">Hello Again</h1>
                 <p className="text-sm text-center">Welcome back you've been missed!</p>
                 <div className="flex flex-col">
- 
+
                     <label className="text-base font-medium mt-1" htmlFor="email">Email</label>
-                    <input id="email" className=" rounded-md p-2 my-1 outline-none" placeholder="abc@email.com" type="email" required onChange={(e)=>setEmail(e.target.value)}/>
+                    <input id="email" className=" rounded-md p-2 my-1 outline-none" placeholder="abc@email.com" type="email" required onChange={(e) => setEmail(e.target.value)} />
 
 
                     <label className="text-base font-medium mt-1" htmlFor="password">Password</label>
-                    <input id="password" className="rounded-md p-2 my-1 outline-none" placeholder="password" type={showPass?'password':'text'}  required onChange={(e)=>setPassword(e.target.value)}/>
+                    <input id="password" className="rounded-md p-2 my-1 outline-none" placeholder="password" type={showPass ? 'password' : 'text'} required onChange={(e) => setPassword(e.target.value)} />
 
                     <div onClick={handleShowPass} className="text-xs cursor-pointer text-blue-600">
-                      { !showPass ?'Hide Password':'show Password'}
-                        </div>
+                        {!showPass ? 'Hide Password' : 'show Password'}
+                    </div>
 
-                    <div className="w-full" onClick={getData}>
+                    <div className="w-full" onClick={loginUser}>
 
-                    <Button text={"Login"}/>
+                        <Button text={"Login"} />
                     </div>
 
                 </div>
